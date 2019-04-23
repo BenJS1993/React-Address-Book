72822bdf1e81265c7c94a4962f22c897ebd70f59
CN 8 <benjs10193@gmail.com>
2019-04-23 16:19:32 +0100

    added a form js file

--- src/App.js
+++ src/App.js
@@ -5,22 +5,11 @@ function App() {
   return (
     <div className="App">
       <body className="addressBookBody">
-      <h1 className="titleText">Address Book</h1>
+        <text>
+          <h1 className="titleText">Address Book{"\n"}</h1>
+          <h2 className="contactsText">List of Contacts</h2>
+        </text>
       </body>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
