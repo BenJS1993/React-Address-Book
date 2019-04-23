65cfb52f2e7d2087014eabd3c893bcc268ff1a14
CN 8 <benjs10193@gmail.com>
2019-04-23 16:55:48 +0100

    edited form and app js files.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './Forms.js';
 
 function App() {
   return (
--- src/Forms.js
+++ src/Forms.js
@@ -6,16 +6,20 @@ class Forms extends Component {
     render() {
 
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>Name:</label>
-                <input type="text" name="firstname" value={this.props.firstname} onChange={this.handleChange.bind(this)} />
-                <label>Surname:</label>
-                <input type="text" name="lastname" value={this.props.lastname} onChange={this.handleChange.bind(this)} />
-                <label>Phone:</label>
-                <input type="text" name="phone" value={this.props.phone} onChange={this.handleChange.bind(this)} />
-                <label>E-mail:</label>
-                <input type="text" name="email" value={this.props.email} onChange={this.handleChange.bind(this)} />
-            </form>
+            <div className="formAddress">
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name:</label>
+                        <input type="text" name="firstname" value={this.props.firstname} onChange={this.handleChange.bind(this)} />
+                    <label>Surname:</label>
+                        <input type="text" name="lastname" value={this.props.lastname} onChange={this.handleChange.bind(this)} />
+                    <label>Phone:</label>
+                        <input type="text" name="phone" value={this.props.phone} onChange={this.handleChange.bind(this)} />
+                    <label>E-mail:</label>
+                        <input type="text" name="email" value={this.props.email} onChange={this.handleChange.bind(this)} />
+                </form>
+            </div>
         )
     }
 }
+
+export default Forms;
