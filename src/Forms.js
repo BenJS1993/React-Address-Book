c1814d5ad712f80d41cf8affcad251f66ca498bc
CN 8 <benjs10193@gmail.com>
2019-04-24 16:16:20 +0100

    address book updates

--- src/Forms.js
+++ src/Forms.js
@@ -9,13 +9,14 @@ class Forms extends Component {
             <div className="formAddress">
                 <form onSubmit={this.handleSubmit}>
                     <label>Name:</label>
-                        <input type="text" name="firstname" value={this.props.firstname} onChange={this.handleChange.bind(this)} />
+                        <input type="text" name="firstname" placeholder="Enter First Name" value={this.props.firstname} onChange={this.handleChange.bind(this)} />
                     <label>Surname:</label>
-                        <input type="text" name="lastname" value={this.props.lastname} onChange={this.handleChange.bind(this)} />
+                        <input type="text" name="lastname" placeholder="Enter Surname" value={this.props.lastname} onChange={this.handleChange.bind(this)} />
                     <label>Phone:</label>
-                        <input type="text" name="phone" value={this.props.phone} onChange={this.handleChange.bind(this)} />
+                        <input type="text" name="phone" placeholder="Enter Phone Number" value={this.props.phone} onChange={this.handleChange.bind(this)} />
                     <label>E-mail:</label>
-                        <input type="text" name="email" value={this.props.email} onChange={this.handleChange.bind(this)} />
+                        <input type="text" name="email" placeholder="Enter Email Address" value={this.props.email} onChange={this.handleChange.bind(this)} />
+                        <button class="formButton" id="formBtn" type="submit">Add Contact</button>
                 </form>
             </div>
         )
